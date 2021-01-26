b0621f802cd19850166efa47fb908d99615b0431
Вирстюк Михаил <m.vi@live.com>
2021-01-26 10:47:47 +0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace InterviewTask17
+{
+    class Program
+    {
+        private static int[] N = new int[] { 1, 2, 3, 0, 5, 6, 7, 8, 9 };
+        private static int M = 5;
+        private static int rez = 1;
+        private static string[] Answers = new String[] { "No", "Yes" };
+
+        static void Main(string[] args)
+        {
+            bool b = IsZero(0);
+            int a = b2i(b);
+            Console.WriteLine("Array contains zeros - " + Answers[a]);
+        }
+
+        static bool IsZero(int n)
+        {
+            return !i2b(M - n) && (i2b(rez *= N[n]) || IsZero(n + 1));
+        }
+
+        static bool i2b(int i)
+        {
+            bool b;
+            unsafe
+            {
+                b = !*(bool*)&i;
+            }
+            return b;
+        }
+
+        static int b2i(bool b)
+        {
+            int i;
+            unsafe
+            {
+                i = *(int*)&b;
+            }
+            return i;
+        }
+    }
+}
