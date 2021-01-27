9bd9f5a99c286771e4dc6fd8f87429a4d3f85c4c
Вирстюк Михаил <m.vi@live.com>
2021-01-27 13:09:02 +0500

    Просто коммит

--- Program.cs
+++ Program.cs
@@ -4,50 +4,31 @@ namespace InterviewTask17
 {
     class Program
     {
-        private static int[] N = new int[] { 1, 2, 3, 0, 5, 6, 7, 8, 9 };
+        private static int[] N = new int[] { 1, -2, 3, 0, 5, 6, 7, 8, 9 };
         private static int M = 5;
         private static int rez = 1;
-        private static string[] Answers = new String[] { "No", "Yes" };
-        static Action<int>[] methods = new Action<int>[] { IsZero, Non };
+        private static string[] Answers = new String[] { "Yes", "No" };
+        static Action<int>[] methods = new Action<int>[] { Non, IsZero };
 
         static void Main(string[] args)
-        {
+        {                      
             IsZero(0);
-            int a = b2i(i2b(rez));
+            int a = (rez * rez).CompareTo(0);
             Console.WriteLine("Array contains zeros - " + Answers[a]);
         }
 
         static void IsZero(int n)
         {
             rez *= N[n];
-            int t = (M - 1 - n) * rez;
-            t = b2i(i2b(t));
-            methods[t].Invoke(n + 1);
+            n++;
+            int t = M / n;
+            t = t.CompareTo(0) * (rez*rez).CompareTo(0);
+            methods[t].Invoke(n);
         }
 
         static void Non(int n)
         {
 
-        }
-
-        static bool i2b(int i)
-        {
-            bool b;
-            unsafe
-            {
-                b = !*(bool*)&i;
-            }
-            return b;
-        }
-
-        static int b2i(bool b)
-        {
-            int i;
-            unsafe
-            {
-                i = *(int*)&b;
-            }
-            return i;
-        }
+        }       
     }
 }
